05405b05929aa17535cbd653b30fe8bf4351cf13
bowenwen <bowenwen@gmail.com>
2017-03-23 18:59:20 -0400

    updated folder structure, swagger spec and readme. add *

---
+++ api/swagger_server/models/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+# import models into model package
+from .error import Error
+from .response_object import ResponseObject
---
+++ api/swagger_server/test/test_historical_travel_times_controller.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from swagger_server.models.error import Error
+from swagger_server.models.response_object import ResponseObject
+from . import BaseTestCase
+from six import BytesIO
+from flask import json
+
+
+class TestHistoricalTravelTimesController(BaseTestCase):
+    """ HistoricalTravelTimesController integration test stubs """
+
+    def test_historical_get(self):
+        """
+        Test case for historical_get
+
+        Historical data of travel times.
+        """
+        query_string = [('start_point', 'start_point_example'),
+                        ('end_point', 'end_point_example'),
+                        ('start_datetime', '2013-10-20'),
+                        ('end_datetime', '2013-10-20')]
+        response = self.client.open('/v0/historical',
+                                    method='GET',
+                                    query_string=query_string)
+        self.assert200(response, "Response body is : " + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
